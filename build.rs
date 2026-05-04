b61427bf1ffe01136e6c74671c2c4da852e761ab
bcaillabet <bcaillabet@gmail.com>
2026-05-04 21:43:03 +0200

    fix: remove license-file, explicit build_server/build_client in build.rs

--- build.rs
+++ build.rs
@@ -1,4 +1,7 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_prost_build::compile_protos("proto/kolektor/v1/config.proto")?;
+    tonic_prost_build::configure()
+        .build_server(true)
+        .build_client(true)
+        .compile_protos(&["proto/kolektor/v1/config.proto"], &["proto"])?;
     Ok(())
 }
