c00db9eaaccb5f2468ceea12f0dd28f0ed0c8fb8
bcaillabet <bcaillabet@gmail.com>
2026-05-04 21:58:47 +0200

    feat: add server/client feature flags to gate proto code generation

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_prost_build::configure()
-        .build_server(true)
-        .build_client(true)
+        .build_server(cfg!(feature = "server"))
+        .build_client(cfg!(feature = "client"))
         .compile_protos(&["proto/kolektor/v1/config.proto"], &["proto"])?;
     Ok(())
 }
